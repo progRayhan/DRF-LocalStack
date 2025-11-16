44dede36ac05710af3bfbdcd75fd696638a2c4c7
Mohammad Rayhan <rayhanul.goni@upaybd.com>
2025-11-16 14:47:20 +0600

    feat: setup minio as localstack web ui

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -58,13 +58,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_DB"),
-        "USER": os.environ.get("POSTGRES_USER"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": os.environ.get("POSTGRES_HOST"),
-        "PORT": os.environ.get("POSTGRES_PORT", 5432),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'drflocalstackdb'),
+        'USER': os.environ.get('POSTGRES_USER', 'drflocalstackuser'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'drflocalstackpassword'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'postgres'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
