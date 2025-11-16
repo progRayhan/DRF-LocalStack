14ea08efcdac878d7cd8cf809df6cfa1239046ac
Mohammad Rayhan <rayhanul.goni@upaybd.com>
2025-11-16 12:08:34 +0600

    feat: s3storage app created

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -9,7 +9,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -17,6 +17,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+THIRD_PARTY_APPS = [
+    'rest_framework',
+    'storages',
+]
+
+LOCAL_APPS = [
+    's3storage',
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
