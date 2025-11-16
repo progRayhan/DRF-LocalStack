a653acc8a8451de9ddd22d485efd324f6bf14b37
Mohammad Rayhan <rayhanul.goni@upaybd.com>
2025-11-16 12:14:01 +0600

    feat: create Document model

--- backend/s3storage/admin.py
+++ backend/s3storage/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Document
 
-# Register your models here.
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ("original_name", "file_size", "content_type", "created_at")
+    list_filter = ("content_type", "created_at")
+    search_fields = ("original_name",)
+    
+    readonly_fields = ("original_name", "file_size", "content_type", "created_at")
+
+    fieldsets = (
+        ("File", {
+            "fields": ("file",)
+        }),
+        ("Metadata", {
+            "fields": ("original_name", "file_size", "content_type")
+        }),
+        ("Timestamps", {
+            "fields": ("created_at",)
+        }),
+    )
--- backend/s3storage/models.py
+++ backend/s3storage/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Document(models.Model):
+    # Upload ANY file type to S3
+    file = models.FileField(upload_to="documents/%Y/%m/%d/")
+
+    # Optional metadata
+    original_name = models.CharField(max_length=255)
+    file_size = models.BigIntegerField()
+    content_type = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        # Populate metadata automatically
+        if self.file:
+            self.original_name = self.file.name
+            self.file_size = self.file.size
+            self.content_type = getattr(self.file.file, "content_type", "")
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.original_name
